c46919538513219b6aab2d321b3a335438fe42be
Tanamay singh <98833259+tanamaay@users.noreply.github.com>
2024-04-02 10:27:03 +0530

    Add files via upload

---
+++ crudapp/src/data/index.js
@@ -0,0 +1,14 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Susan',
+    lastName: 'singh',
+    email: 'susan@example.com',
+    salary: '95000',
+    date: '2024-03-28',
+  },
+ 
+  
+];
+
+export { employeesData };
